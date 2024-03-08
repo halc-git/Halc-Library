33ea32e0c82eae180a6d987e4ab0a9500c3b9b56
halc_kyopro <hirayuu3229@outlook.jp>
2024-03-08 20:48:17 +0900

    modified:   Verify/verify-yuki/yuki-1092.test.cpp

--- Verify/verify-yuki/yuki-1092.test.cpp
+++ Verify/verify-yuki/yuki-1092.test.cpp
@@ -1,4 +1,4 @@
-#define PROBREM "https://yukicoder.me/problems/no/336"
+#define PROBREM "https://yukicoder.me/problems/no/1092"
 #include"../../Template.hpp"
 #include"../../Modint/MontgomeryModint.hpp"
 using mint=DynamicMontgomeryModint<0>;
