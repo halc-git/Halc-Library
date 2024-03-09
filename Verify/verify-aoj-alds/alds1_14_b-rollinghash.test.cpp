fce0a600eda998d58c0bcdf3df88891ab699225d
halc_kyopro <hirayuu3229@outlook.jp>
2024-03-09 22:03:48 +0900

    add rollinghash-verify

---
+++ Verify/verify-aoj-alds/alds1_14_b-rollinghash.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+#include"../../Template.hpp"
+#include"../../String/RollingHash.hpp"
+int main(){
+    STR(T);
+    STR(P);
+    RollingHash<string> roll;
+    auto tb=roll.build(T);
+    auto pb=roll.build(P);
+    ll hash=roll.query(pb,0,len(P));
+    rep(i,len(T)-len(P)+1){
+        if(roll.query(tb,i,i+len(P))==hash)out(i);
+    }
+}
