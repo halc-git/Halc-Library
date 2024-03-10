cd9b8a688030035088c7f11dfce991ff89ac2356
halc_kyopro <hirayuu3229@outlook.jp>
2024-03-10 09:07:23 +0900

    add FoldableDeque

--- DataStructure/FoldableDeque.hpp
+++ DataStructure/FoldableDeque.hpp
@@ -13,7 +13,7 @@ struct FoldableQueue{
         bottomfold.push(M::op(bottomfold.top(),v));
     }
     void pop(){
-        if(top.size()==0){
+        if(top.empty()){
             while(!bottom.empty()){
                 top.push(bottom.top());
                 topfold.push(M::op(bottom.top(),topfold.top()));
@@ -25,7 +25,7 @@ struct FoldableQueue{
         topfold.pop();
     }
     T front(){
-        if(top.size()==0){
+        if(top.empty()){
             while(!bottom.empty()){
                 top.push(bottom.top());
                 topfold.push(M::op(bottom.top(),topfold.top()));
@@ -34,6 +34,114 @@ struct FoldableQueue{
             }
         }
         return top.top();
+    }
+    T get_all(){
+        return M::op(topfold.top(),bottomfold.top());
+    }
+    size_t size(){
+        return top.size()+bottom.size();
+    }
+    bool empty(){
+        return top.empty()&&bottom.empty();
+    }
+};
+template<class M>
+struct FoldableDeque{
+    using T=typename M::T;
+    stack<T> top,bottom,topfold,bottomfold;
+    FoldableDeque(){
+        topfold.push(M::e);
+        bottomfold.push(M::e);
+    }
+    void push_front(T v){
+        top.push(v);
+        topfold.push(M::op(v,topfold.top()));
+    }
+    void push_back(T v){
+        bottom.push(v);
+        bottomfold.push(M::op(bottomfold.top(),v));
+    }
+    void pop_front(){
+        if(top.empty()){
+            vector<T> change;
+            while(!bottom.empty()){
+                change.emplace_back(bottom.top());
+                bottom.pop();
+                bottomfold.pop();
+            }
+            size_t sz=change.size();
+            for(int32_t i=(sz>>1); i>=0; i--){
+                top.push(change[sz-i-1]);
+                topfold.push(M::op(change[sz-i-1],topfold.top()));
+            }
+            for(int32_t i=(sz>>1)+1; i<sz; i++){
+                bottom.push(change[sz-i-1]);
+                bottomfold.push(M::op(bottomfold.top(),change[sz-i-1]));
+            }
+        }
+        top.pop();
+        topfold.pop();
+    }
+    void pop_back(){
+        if(bottom.empty()){
+            vector<T> change;
+            while(!top.empty()){
+                change.emplace_back(top.top());
+                top.pop();
+                topfold.pop();
+            }
+            size_t sz=change.size();
+            for(int32_t i=(sz>>1); i>=0; i--){
+                bottom.push(change[sz-i-1]);
+                bottomfold.push(M::op(bottomfold.top(),change[sz-i-1]));
+            }
+            for(int32_t i=(sz>>1)+1; i<sz; i++){
+                top.push(change[sz-i-1]);
+                topfold.push(M::op(change[sz-i-1],topfold.top()));
+            }
+        }
+        bottom.pop();
+        bottomfold.pop();
+    }
+    T front(){
+        if(top.empty()){
+            vector<T> change;
+            while(!bottom.empty()){
+                change.emplace_back(bottom.top());
+                bottom.pop();
+                bottomfold.pop();
+            }
+            size_t sz=change.size();
+            for(uint32_t i=(sz>>1); i>=0; i--){
+                top.push(change[i]);
+                topfold.push(M::op(change[i],topfold.top()));
+            }
+            for(uint32_t i=(sz>>1)+1; i<sz; i++){
+                bottom.push(change[i]);
+                bottomfold.push(M::op(bottomfold.top(),change[i]));
+            }
+        }
+        return top.top();
+    }
+    T back(){
+        if(bottom.empty()){
+            vector<T> change;
+            while(!top.empty()){
+                change.emplace_back(top.top());
+                top.pop();
+                topfold.pop();
+            }
+            size_t sz=change.size();
+            for(uint32_t i=(sz>>1); i>=0; i--){
+                bottom.push(change[i]);
+                bottomfold.push(M::op(bottomfold.top(),change[i]));
+            }
+            for(uint32_t i=(sz>>1)+1; i<sz; i++){
+                top.push(change[i]);
+                topfold.push(M::op(change[i],topfold.top()));
+            }
+        }
+        return bottom.top();
     }
     T get_all(){
         return M::op(topfold.top(),bottomfold.top());
---
+++ Verify/verify-yosupo-datastructure/deque_operate_all_composite.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/deque_operate_all_composite"
+#include"../../Template.hpp"
+#include"../../DataStructure/FoldableDeque.hpp"
+struct composite{
+    using T=pll;
+    static T op(T lf,T ri){
+        return T((lf.fi*ri.fi)%MOD,(lf.se*ri.fi+ri.se)%MOD);
+    }
+    static inline T e=pll(1,0);
+};
+int main(){
+    LL(Q);
+    FoldableDeque<composite> deq;
+    rep(i,Q){
+        LL(t);
+        if(t==0){
+            LL(a,b);
+            deq.push_front(pll(a,b));
+        }
+        if(t==1){
+            LL(a,b);
+            deq.push_back(pll(a,b));
+        }
+        if(t==2){
+            deq.pop_front();
+        }
+        if(t==3){
+            deq.pop_back();
+        }
+        if(t==4){
+            LL(x);
+            pll ope=deq.get_all();
+            out((ope.fi*x+ope.se)%MOD);
+        }
+    }
+}
