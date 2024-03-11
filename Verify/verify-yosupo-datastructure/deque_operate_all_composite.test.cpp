bc81e2c64996a5de96bb99f3665a82b246c2ba13
halc_kyopro <hirayuu3229@outlook.jp>
2024-03-11 19:31:34 +0900

    modified:   Verify/verify-yosupo-datastructure/deque_operate_all_composite.test.cpp

--- Verify/verify-yosupo-datastructure/deque_operate_all_composite.test.cpp
+++ Verify/verify-yosupo-datastructure/deque_operate_all_composite.test.cpp
@@ -1,8 +1,8 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/deque_operate_all_composite"
 #include"../../Template.hpp"
 #include"../../DataStructure/FoldableDeque.hpp"
-#include"../../Math/MontgomeryModint.hpp"
-using mint=MontgomeryModint<MOD>;
+#include"../../Math/BarrettModint.hpp"
+using mint=BarrettModint<MOD>;
 struct composite{
     using T=pair<mint,mint>;
     static T op(T lf,T ri){
