cc4be59cf8f2008c1d7fdd2910331ad6dc8028ab
halc_kyopro <hirayuu3229@outlook.jp>
2024-04-21 12:08:28 +0900

    modified:   Heuristic.hpp

--- Heuristic.hpp
+++ Heuristic.hpp
@@ -1,12 +1,16 @@
+#pragma once
 using namespace chrono;
-unsigned int randxor(){
-    static uint32_t x=42;
-    x=x^(x<<13);x=x^(x>>15);x=x^(x<<17);
-    return x;
+uint32_t pcg32_fast(){
+    static uint64_t state=(steady_clock::now().time_since_epoch().count()<<1)+1;
+    uint64_t x=state;
+    uint8_t count=x>>61;
+    state*=0xf13283ad;
+    x^=x>>22;
+    return (uint32_t)(x>>(22+count));
 }
 
 int32_t randint(int32_t l,int32_t r){
-    return l+(((long long)randxor()*(r-l+1))>>32);
+    return l+(((int64_t)pcg32_fast()*(r-l+1))>>32);
 }
 
 auto startTime=system_clock::now();
