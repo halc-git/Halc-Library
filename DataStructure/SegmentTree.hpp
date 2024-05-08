e6ff304ddb3fe4d52099749382c8d21cb1038c05
halc_kyopro <hirayuu3229@outlook.jp>
2024-05-08 22:46:15 +0900

    add bisect in

--- DataStructure/SegmentTree.hpp
+++ DataStructure/SegmentTree.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <cstdint>
 #include <vector>
 template <class M>
@@ -8,11 +9,11 @@ struct SegmentTree {
     std::vector<T> tree;
     SegmentTree(int32_t sz) {
         siz = sz;
-        tree = std::vector<T>(siz * 2, M::e);
+        tree = std::vector<T>(siz << 1, M::e);
     }
     SegmentTree(std::vector<T> def) {
         siz = def.size();
-        tree = vector<T>(siz * 2, M::e);
+        tree = vector<T>(siz << 1, M::e);
         for (int32_t i = 0; i < siz; i++) {
             tree[i + siz] = def[i];
         }
@@ -47,6 +48,110 @@ struct SegmentTree {
             ri >>= 1;
         }
         return M::op(rel, rer);
+    }
+    template <bool (*f)(T)>
+    int32_t max_right(int lf) {
+        return max_right(lf, [](T x) { return f(x); });
+    }
+    template <class F>
+    int32_t max_right(int32_t lf, F f) {
+        lf += siz;
+        int32_t ri = siz << 1;
+        std::vector<int32_t> lfp, rip;
+        while (lf < ri) {
+            if (lf & 1) {
+                lfp.emplace_back(lf);
+                lf++;
+            }
+            if (ri & 1) {
+                ri--;
+                rip.emplace_back(ri);
+            }
+            lf >>= 1;
+            ri >>= 1;
+        }
+        T val = M::e;
+        for (int32_t i : lfp) {
+            if (!f(M::op(val, tree[i]))) {
+                while (i < siz) {
+                    i <<= 1;
+                    if (f(M::op(val, tree[i]))) {
+                        val = M::op(val, tree[i]);
+                        i++;
+                    }
+                }
+                return i - siz;
+            }
+            val = M::op(val, tree[i]);
+        }
+        std::reverse(rip.begin(), rip.end());
+        for (int32_t i : rip) {
+            if (!f(M::op(val, tree[i]))) {
+                while (i < siz) {
+                    i <<= 1;
+                    if (f(M::op(val, tree[i]))) {
+                        val = M::op(val, tree[i]);
+                        i++;
+                    }
+                }
+                return i - siz;
+            }
+            val = M::op(val, tree[i]);
+        }
+        return siz;
+    }
+    template <bool (*f)(T)>
+    int32_t min_left(int ri) {
+        return min_left(ri, [](T x) { return f(x); });
+    }
+    template <class F>
+    int32_t min_left(int32_t ri, F f) {
+        ri += siz;
+        int32_t lf = siz;
+        std::vector<int32_t> lfp, rip;
+        while (lf < ri) {
+            if (lf & 1) {
+                lfp.emplace_back(lf);
+                lf++;
+            }
+            if (ri & 1) {
+                ri--;
+                rip.emplace_back(ri);
+            }
+            lf >>= 1;
+            ri >>= 1;
+        }
+        T val = M::e;
+        for (int32_t i : rip) {
+            if (!f(M::op(val, tree[i]))) {
+                while (i < siz) {
+                    i <<= 1;
+                    i++;
+                    if (f(M::op(tree[i], val))) {
+                        val = M::op(tree[i], val);
+                        i--;
+                    }
+                }
+                return i - siz + 1;
+            }
+            val = M::op(tree[i], val);
+        }
+        std::reverse(lfp.begin(), lfp.end());
+        for (int32_t i : lfp) {
+            if (!f(M::op(val, tree[i]))) {
+                while (i < siz) {
+                    i <<= 1;
+                    i++;
+                    if (f(M::op(tree[i], val))) {
+                        val = M::op(tree[i], val);
+                        i--;
+                    }
+                }
+                return i - siz + 1;
+            }
+            val = M::op(tree[i], val);
+        }
+        return 0;
     }
     size_t size() { return siz; }
 };
