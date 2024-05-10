7ec831c27a2a004ef8e86a23de36240ee20a5973
halc_kyopro <hirayuu3229@outlook.jp>
2024-05-10 16:54:59 +0900

    fix inout

--- Template/InOut.hpp
+++ Template/InOut.hpp
@@ -1,28 +1,27 @@
 #pragma once
 #include <array>
-#include <cstdio>
 #include <deque>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
-inline int scan() { return getchar(); }
-inline void scan(int &a) { scanf("%d", &a); }
-inline void scan(unsigned &a) { scanf("%u", &a); }
-inline void scan(long &a) { scanf("%ld", &a); }
-inline void scan(long long &a) { scanf("%lld", &a); }
-inline void scan(unsigned long long &a) { scanf("%llu", &a); }
+inline void scan() {}
+inline void scan(int &a) { std::cin >> a; }
+inline void scan(unsigned &a) { std::cin >> a; }
+inline void scan(long &a) { std::cin >> a; }
+inline void scan(long long &a) { std::cin >> a; }
+inline void scan(unsigned long long &a) { std::cin >> a; }
 inline void scan(char &a) { std::cin >> a; }
-inline void scan(float &a) { scanf("%f", &a); }
-inline void scan(double &a) { scanf("%lf", &a); }
-inline void scan(long double &a) { scanf("%Lf", &a); }
+inline void scan(float &a) { std::cin >> a; }
+inline void scan(double &a) { std::cin >> a; }
+inline void scan(long double &a) { std::cin >> a; }
 inline void scan(std::vector<bool> &vec) {
-    for (unsigned i = 0; i < vec.size(); i++) {
+    for (int32_t i = 0; i < vec.size(); i++) {
         int a;
         scan(a);
         vec[i] = a;
     }
 }
-inline void scan(char a[]) { scanf("%s", a); }
 inline void scan(std::string &a) { std::cin >> a; }
 template <class T>
 inline void scan(std::vector<T> &vec);
@@ -63,18 +62,18 @@ inline void in(Head &head, Tail &...tail) {
     scan(head);
     in(tail...);
 }
-inline void print() { putchar(' '); }
-inline void print(const bool &a) { printf("%d", a); }
-inline void print(const int &a) { printf("%d", a); }
-inline void print(const unsigned &a) { printf("%u", a); }
-inline void print(const long &a) { printf("%ld", a); }
-inline void print(const long long &a) { printf("%lld", a); }
-inline void print(const unsigned long long &a) { printf("%llu", a); }
-inline void print(const char &a) { printf("%c", a); }
-inline void print(const char a[]) { printf("%s", a); }
-inline void print(const float &a) { printf("%.15f", a); }
-inline void print(const double &a) { printf("%.15f", a); }
-inline void print(const long double &a) { printf("%.15Lf", a); }
+inline void print() { std::cout << ' '; }
+inline void print(const bool &a) { std::cout << a; }
+inline void print(const int &a) { std::cout << a; }
+inline void print(const unsigned &a) { std::cout << a; }
+inline void print(const long &a) { std::cout << a; }
+inline void print(const long long &a) { std::cout << a; }
+inline void print(const unsigned long long &a) { std::cout << a; }
+inline void print(const char &a) { std::cout << a; }
+inline void print(const char a[]) { std::cout << a; }
+inline void print(const float &a) { std::cout << a; }
+inline void print(const double &a) { std::cout << a; }
+inline void print(const long double &a) { std::cout << a; }
 inline void print(const std::string &a) {
     for (auto &&i : a) print(i);
 }
@@ -91,7 +90,7 @@ inline void print(const std::vector<T> &vec) {
     if (vec.empty()) return;
     print(vec[0]);
     for (auto i = vec.begin(); ++i != vec.end();) {
-        putchar(' ');
+        std::cout << ' ';
         print(*i);
     }
 }
@@ -100,7 +99,7 @@ inline void print(const std::deque<T> &vec) {
     if (vec.empty()) return;
     print(vec[0]);
     for (auto i = vec.begin(); ++i != vec.end();) {
-        putchar(' ');
+        std::cout << ' ';
         print(*i);
     }
 }
@@ -108,21 +107,21 @@ template <class T, size_t size>
 inline void print(const std::array<T, size> &vec) {
     print(vec[0]);
     for (auto i = vec.begin(); ++i != vec.end();) {
-        putchar(' ');
+        std::cout << ' ';
         print(*i);
     }
 }
 template <class T, class L>
 inline void print(const std::pair<T, L> &p) {
     print(p.first);
-    putchar(' ');
+    std::cout << ' ';
     print(p.second);
 }
 template <class T, size_t size>
 inline void print(const T (&vec)[size]) {
     print(vec[0]);
     for (auto i = vec; ++i != end(vec);) {
-        putchar(' ');
+        std::cout << ' ';
         print(*i);
     }
 }
@@ -139,8 +138,15 @@ inline void out(const T &t) {
 template <class Head, class... Tail>
 inline void out(const Head &head, const Tail &...tail) {
     print(head);
-    putchar(' ');
+    std::cout << ' ';
     out(tail...);
 }
 inline void Yes(bool i = true) { out(i ? "Yes" : "No"); }
 inline void No(bool i = true) { out(i ? "No" : "Yes"); }
+struct IOsetup {
+    IOsetup() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout << std::setprecision(10);
+    }
+} iosetup;
