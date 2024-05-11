847c484478da7d881126ca7a64e4e9f2970db1a1
halc-git <141024429+halc-git@users.noreply.github.com>
2024-05-11 13:42:19 +0900

    Create aplusb-test_timer.cpp

---
+++ Verify/verify-yosupo-sample/aplusb-test_timer.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include "../../Misc/Timer.hpp"
+#include "../../Misc/Random.hpp"
+#include "../../Template/Template.hpp"
+int main() {
+    int stoptime = randint(0, 1000000);
+    usleep(stoptime);
+    assert(abs(get_time() - stoptime) <= 500);
+    LL(A, B);
+    out(A + B);
+}
