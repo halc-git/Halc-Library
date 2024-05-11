1745870ff3049fba6d96e4af51398479dc590e1f
halc-git <141024429+halc-git@users.noreply.github.com>
2024-05-11 13:30:23 +0900

    Update Timer.hpp

--- Misc/Timer.hpp
+++ Misc/Timer.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include <chrono>
-static auto startTime = std::chrono::system_clock::now();
-inline int32_t getTime() {
+static auto StartTime = std::chrono::system_clock::now();
+inline int32_t get_time() {
     return std::chrono::duration_cast<std::chrono::microseconds>(
-               std::chrono::system_clock::now() - startTime)
+               std::chrono::system_clock::now() - StartTime)
         .count();
 }
