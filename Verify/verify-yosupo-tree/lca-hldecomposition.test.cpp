38859e477d47a6f483ddada926075753ff80bba8
halc_kyopro <hirayuu3229@outlook.jp>
2024-05-12 20:01:30 +0900

    fix lca

--- Verify/verify-yosupo-tree/lca-hldecomposition.test.cpp
+++ Verify/verify-yosupo-tree/lca-hldecomposition.test.cpp
@@ -1,16 +1,16 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/lca"
-#include"../../Template/Template.hpp"
-#include"../../Graph/Graph.hpp"
-#include"../../Tree/HLDecomposition.hpp"
-void solve(){
+#include "../../Graph/Graph.hpp"
+#include "../../Tree/HLDecomposition.hpp"
+#include "../../Template/Template.hpp"
+void solve() {
     LL(N, Q);
     Graph gr(N);
     VEC(ll, p, N - 1);
     rep(i, N - 1) { gr.add_edge(i + 1, p[i]); }
     HLDecomposition hld(gr);
-    rep(i,Q){
-        LL(u,v);
-        out(hld.lca(u,v));
+    rep(i, Q) {
+        LL(u, v);
+        out(hld.lca(u, v));
     }
 }
-int main(){ solve(); }
+int main() { solve(); }
