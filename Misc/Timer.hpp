56ef419bf9561fe58adc5a4cc19509b4a71d2701
halc_kyopro <hirayuu3229@outlook.jp>
2024-06-20 22:25:44 +0900

    delete heur library

--- Misc/Timer.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <chrono>
-static auto StartTime = std::chrono::system_clock::now();
-inline int32_t get_time() {
-    return std::chrono::duration_cast<std::chrono::microseconds>(
-               std::chrono::system_clock::now() - StartTime)
-        .count();
-}
