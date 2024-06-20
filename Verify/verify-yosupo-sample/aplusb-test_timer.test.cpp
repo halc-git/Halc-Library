5b1e7233cf9ed7074ab5c0e2bf1ce4b632d45267
halc_kyopro <hirayuu3229@outlook.jp>
2024-06-20 20:50:32 +0900

    fix random

--- Verify/verify-yosupo-sample/aplusb-test_timer.test.cpp
+++ Verify/verify-yosupo-sample/aplusb-test_timer.test.cpp
@@ -5,7 +5,7 @@
 int main() {
     int stoptime = randint(0, 1000000);
     usleep(stoptime);
-    assert(abs(get_time() - stoptime) <= 500);
+    assert(abs(get_time() - stoptime) <= 1000);
     LL(A, B);
     out(A + B);
 }
